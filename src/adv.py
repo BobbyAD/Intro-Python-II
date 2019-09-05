5fce570141bf6259ea2c2ce5e13913b33cf75d4e
Robert Driskell <driskell.ba@gmail.com>
2019-09-05 18:02:30 -0500

    items works

--- src/adv.py
+++ src/adv.py
@@ -1,25 +1,30 @@
 from room import Room
 from player import Player
+from item import Item
 
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                    "North of you, the cave mount beckons"),
+                    "North of you, the cave mouth beckons",
+                    [
+                        Item("Sword", "A sword, it's a little rusty"), 
+                        Item("Staff", "A staff, basically just a stick")
+                    ]),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", [Item("Rocks", "Some rocks, nothing special.")]),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", [Item("Broken Lantern", "It smells of old oil")]),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", []),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", [Item("Coin", "One side has a face you don't recognize. The other has an eagle.")]),
 }
 
 
@@ -50,19 +55,47 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+name = input("What is your name? ")
+player = Player(name, room['outside'], [])
 
-player = Player("Name", room['outside'])
+action = ''
 
-i = ''
+print(f"\n{player.current_room}")
 
-while i is not 'q':
-    i = input("Enter a direction: ")
-    if i == 'n' or i == 's' or i == 'e' or i == 'w':
-        print(i)
-        print(player.move(i))
-    elif i == 'q':
-        print("Cya")
+while action is not 'q':
+    action = input("Enter an action: ").strip().lower().split(" ")
+    if len(action) == 1:
+        if action[0] == 'n' or action[0] == 's' or action[0] == 'e' or action[0] == 'w':
+            print(player.move(action[0]) + "\n")
+        elif action[0] == 'q':
+            if input("Are you sure? (y/n) ") != 'y':
+                action = ''
+                print()
+            else:
+                action = 'q'
+                print()
+        elif action[0] == 'inventory':
+            player.check_inventory()
+        elif action[0] == 'look':
+            print(f"\n{player.current_room}")
+        else:
+            print("Please input a valid action")
+    elif len(action) == 2:
+        if action[0] == 'get':
+            for i in player.current_room.items:
+                if i.name.lower().split(" ")[0] == action[1]:
+                    player.pickup(i)
+                    action = ''
+                elif len(action) > 0:
+                    print("That item does not exist\n")
+        elif action[0] == 'drop':
+            for i in player.items:
+                if i.name.lower().split(" ")[0] == action[1]:
+                    player.drop(i)
+                elif len(action) > 0:
+                    print("That item does not exist\n")
     else:
-        print("Please pick a valid direction")
+        print("Please input a valid action")
+
 
 print("Thanks for playing")
--- src/room.py
+++ src/room.py
@@ -2,10 +2,20 @@
 # description attributes.
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, items):
         self.name = name
         self.description = description
-    
+        self.items = items
+
+    def __str__(self):
+        print_list = "\n".join([i.__str__() for i in self.items])
+        print_str = ""
+        if len(self.items) > 0:
+            print_str = f"{self.name}\n\n{self.description}\n\nYou can see some items:\n{print_list}\n"
+        else:
+            print_str = f"{self.name}\n\n{self.description}\n"
+        return print_str
+
     n_to = ''
     s_to = ''
     e_to = ''
